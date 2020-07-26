40ed8b960bf3aec219e2d1188232886e86f6816f
William S Chen <blackjamx5@hotmail.com>
2020-07-26 15:46:33 -0700

    basic react-slider implemented

--- frontend/src/Components/entry/sub-components/drag-drop-file.js
+++ frontend/src/Components/entry/sub-components/drag-drop-file.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import ImageSlider from '../../image-carousel';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import PropTypes from 'prop-types';
 import './drag-drop-file.scss';
 
@@ -15,16 +18,16 @@ const DragDropFile = (props) => {
     const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
-        
+
         let filteredArr = selectedFiles.reduce((acc, current) => {
             const x = acc.find(item => item.name === current.name);
             if (!x) {
-              return acc.concat([current]);
+                return acc.concat([current]);
             } else {
-              return acc;
+                return acc;
             }
         }, []);
-        setValidFiles([...filteredArr]);       
+        setValidFiles([...filteredArr]);
     }, [selectedFiles]);
 
     const preventDefault = (e) => {
@@ -64,7 +67,7 @@ const DragDropFile = (props) => {
 
     const handleFiles = (files) => {
         let invalidFound = false;
-        for(let i = 0; i < files.length; i++) {
+        for (let i = 0; i < files.length; i++) {
             if (validateFile(files[i])) {
                 setSelectedFiles(prevArray => [...prevArray, files[i]]);
             } else {
@@ -88,7 +91,7 @@ const DragDropFile = (props) => {
 
     const fileSize = (size) => {
         if (size === 0) {
-          return '0 Bytes';
+            return '0 Bytes';
         }
         const k = 1024;
         const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
@@ -112,10 +115,9 @@ const DragDropFile = (props) => {
             unsupportedFiles.splice(index3, 1);
             setUnsupportedFiles([...unsupportedFiles]);
         }
-        console.log(validFiles.length);
-        console.log(unsupportedFiles.length);
+
         if (validFiles.length > 0 && unsupportedFiles.length === 0) updateDisabilityState(false);
-        else{
+        else {
             updateDisabilityState(true);
         }
     }
@@ -130,7 +132,7 @@ const DragDropFile = (props) => {
         const reader = new FileReader();
         modalRef.current.style.display = "block";
         reader.readAsDataURL(file);
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             modalImageRef.current.style.backgroundImage = `url(${e.target.result})`;
         }
     }
@@ -175,75 +177,116 @@ const DragDropFile = (props) => {
     }
 
     const dropContainer = (
-        <div className="drop-container"
-        onDragOver={dragOver}
-        onDragEnter={dragEnter}
-        onDragLeave={dragLeave}
-        onDrop={fileDrop}
-        onClick={fileInputClicked}
-    >
-        <div className="drop-message">
-            <div className="upload-icon"></div>
+        <div className="drop-image-container"
+            onDragOver={dragOver}
+            onDragEnter={dragEnter}
+            onDragLeave={dragLeave}
+            onDrop={fileDrop}
+            onClick={fileInputClicked}
+        >
+            <div className="drop-message">
+                <div className="upload-icon"></div>
             Drag and Drop files here or click to select file(s)
         </div>
-        <input
-            ref={fileInputRef}
-            className="file-input"
-            type="file"
-            multiple
-            onChange={filesSelected}
-        />
-    </div>
+            <input
+                ref={fileInputRef}
+                className="file-input"
+                type="file"
+                multiple
+                onChange={filesSelected}
+            />
+        </div>
     );
 
     const fileDisplayContainer = (
         <div className="file-display-container">
-        {
-            validFiles.map((data, i) => 
-                <div className="file-status-bar" key={i}>
-                    <div onClick={!data.invalid ? () => openImageModal(data) : () => removeFile(data.name)}>
-                        <div className="file-type-logo"></div>
-                        <div className="file-type">{fileType(data.name)}</div>
-                        <span className={`file-name ${data.invalid ? 'file-error' : ''}`}>{data.name}</span>
-                        <span className="file-size">({fileSize(data.size)})</span> {data.invalid && <span className='file-error-message'>({errorMessage})</span>}
+            {
+                validFiles.map((data, i) =>
+                    <div className="file-status-bar" key={i}>
+                        <div onClick={!data.invalid ? () => openImageModal(data) : () => removeFile(data.name)}>
+                            <div className="file-type-logo"></div>
+                            <div className="file-type">{fileType(data.name)}</div>
+                            <span className={`file-name ${data.invalid ? 'file-error' : ''}`}>{data.name}</span>
+                            <span className="file-size">({fileSize(data.size)})</span> {data.invalid && <span className='file-error-message'>({errorMessage})</span>}
+                        </div>
+                        <div className="file-remove" onClick={() => removeFile(data.name)}>X</div>
                     </div>
-                    <div className="file-remove" onClick={() => removeFile(data.name)}>X</div>
-                </div>
-            )
-        }
-    </div>
+                )
+            }
+        </div>
     );
-    console.log("Valid Files: " + validFiles.length );
-        
-    return (
+
+    const modal = (
+        <div className="modal" ref={modalRef}>
+            <div className="overlay"></div>
+            <span className="close" onClick={(() => closeModal())}>X</span>
+            <div className="modal-image" ref={modalImageRef}></div>
+        </div>
+    );
+
+    const heroModal = (
+        <div className="upload-modal" ref={uploadModalRef}>
+            <div className="overlay"></div>
+            <div className="close" onClick={(() => closeUploadModal())}>X</div>
+            <div className="progress-container">
+                <span ref={uploadRef}></span>
+                <div className="progress">
+                    <div className="progress-bar" ref={progressRef}></div>
+                </div>
+            </div>
+        </div>
+    );
+
+    if (validFiles.length === 0) return (
         <>
             <div className="photo-upload-container">
-                {unsupportedFiles.length === 0 && validFiles.length ? <button className="file-upload-btn" onClick={() => uploadFiles()}>Upload Files</button> : ''} 
+                {/* {unsupportedFiles.length === 0 && validFiles.length ? <button className="file-upload-btn" onClick={() => uploadFiles()}>Upload Files</button> : ''} */}
                 {unsupportedFiles.length ? <p>Please remove all unsupported files.</p> : ''}
-                {dropContainer}       
+                {dropContainer}
                 {fileDisplayContainer}
             </div>
-            <div className="modal" ref={modalRef}>
-                <div className="overlay"></div>
-                <span className="close" onClick={(() => closeModal())}>X</span>
-                <div className="modal-image" ref={modalImageRef}></div>
-            </div>
-
-            <div className="upload-modal" ref={uploadModalRef}>
-                <div className="overlay"></div>
-                <div className="close" onClick={(() => closeUploadModal())}>X</div>
-                <div className="progress-container">
-                    <span ref={uploadRef}></span>
-                    <div className="progress">
-                        <div className="progress-bar" ref={progressRef}></div>
-                    </div>
-                </div>
-            </div>
+            {/* {modal}
+            {heroModal} */}
         </>
     );
+    else {
+        console.log(validFiles);
+        return (
+            <>
+                <div className="photo-upload-container">
+                    {/* {unsupportedFiles.length === 0 && validFiles.length ? <button className="file-upload-btn" onClick={() => uploadFiles()}>Upload Files</button> : ''} */}
+                    {unsupportedFiles.length ? <p>Please remove all unsupported files.</p> : ''}
+                    {/* <div className="drop-image-container"> */}
+                    {    
+                    // validFiles.map((data, i) =>
+                    //     <div className="file-status-bar" key={i}>
+                    //         <div onClick={!data.invalid ? () => openImageModal(data) : () => removeFile(data.name)}>
+                    //             <div className="file-type-logo"></div>
+                    //             <div className="file-type">{fileType(data.name)}</div>
+                    //             <span className={`file-name ${data.invalid ? 'file-error' : ''}`}>{data.name}</span>
+                    //             <span className="file-size">({fileSize(data.size)})</span> {data.invalid && <span className='file-error-message'>({errorMessage})</span>}
+                    //         </div>
+                    //         <div className="file-remove" onClick={() => removeFile(data.name)}>X</div>
+                    //     </div>
+                    //      /* {if not last img in gallery <button/> : '} */
+                    //     /* {if is first in gallery <button/> : '} */
+                    // )
+                    }
+
+                    {/* </div>   */}
+                    <ImageSlider fileArray={validFiles} />
+
+                    {/* {dropContainer}
+                    {fileDisplayContainer} */}
+                </div>
+                    {/* {modal}
+                    {heroModal} */}
+            </>
+        );
+    }
 }
 
- {/* <input
+{/* <input
                     name="files[]"
                     type="file"
                     multiple
---
+++ frontend/src/Components/image-carousel/index.js
@@ -0,0 +1,103 @@
+import React, { useRef } from "react";
+import Slider from "react-slick";
+
+// https://css-tricks.com/centering-css-complete-guide/#center-vertically
+//Explains the weird -50% and top 50% thing
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red", transform: `translate(${100}px, ${-50}%)`, zIndex: 2, top: "50%" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red", transform: `translate(${-100}px, ${-50}%)`, zIndex: 2, top: "50%" }}
+      onClick={onClick}
+    />
+  );
+}
+const settings = {
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
+class ImageSlider extends React.Component {
+  _isMounted = false;
+  constructor(props) {
+    super(props);
+    this.state = {
+      transformedImageArray: null
+    }
+    this.loadImage = this.loadImage.bind(this);
+    this.transformImageProp = this.transformImageProp.bind(this);
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    console.log(this.props.fileArray);
+    if (this._isMounted && this.props.fileArray.length !== 0) this.transformImageProp(); 
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+
+  transformImageProp(){
+    let imageArray = this.props.fileArray;
+    Promise.all(imageArray.map((file) => this.loadImage(file)))
+    .then(
+      newArray =>
+        newArray.map(item =>
+          item = (
+            <div className="photo-container">
+              <img className="photo" src={item} alt='image not loaded' />
+            </div>
+          ))
+    ).then(result => {
+      console.log(result);
+      this.setState({ transformedImageArray: result })
+    });
+  }
+
+  loadImage(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        resolve(e.target.result);
+      };
+
+      reader.onerror = function () {
+        reject(reader.error);
+      }
+      reader.readAsDataURL(file);
+    });
+  }
+  render() {
+
+    const container =
+      (
+        !!this.state.transformedImageArray ? this.state.transformedImageArray : <p>IMAGE IS STILL LOADING</p>
+      );
+    
+    return (
+
+      <Slider {...settings}>
+        {container}
+      </Slider>
+
+    );
+  }
+}
+
+export default ImageSlider;
