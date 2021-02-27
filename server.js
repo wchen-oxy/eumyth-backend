355e7e7701a38fbb7f8f5a909a0ed1a9d9396e0f
William S Chen <blackjamx5@hotmail.com>
2021-02-26 17:05:18 -0800

    moved build

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+app.use(express.static(path.join(__dirname,  "build")));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
