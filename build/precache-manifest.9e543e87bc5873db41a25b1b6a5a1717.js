2cc96c4fd0e209cb0a16bd902dea99579235a91e
William S Chen <blackjamx5@hotmail.com>
2021-03-01 14:32:53 -0800

    added back build

---
+++ build/precache-manifest.9e543e87bc5873db41a25b1b6a5a1717.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd7631977004c4cc9fb4c523529cd4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1296551734bcfc3be64b",
+    "url": "/static/css/2.995fd7aa.chunk.css"
+  },
+  {
+    "revision": "86af6ae4c4c8b80fd2ed",
+    "url": "/static/css/main.9e49eeb3.chunk.css"
+  },
+  {
+    "revision": "1296551734bcfc3be64b",
+    "url": "/static/js/2.886747de.chunk.js"
+  },
+  {
+    "revision": "86af6ae4c4c8b80fd2ed",
+    "url": "/static/js/main.59c1cd6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
