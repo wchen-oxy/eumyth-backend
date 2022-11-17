a0827ea8ac4bcb304b813c911387b8727bd30fd6
William S Chen <blackjamx5@hotmail.com>
2022-11-17 10:01:11 -0800

    modified search routes for better data

--- routes/v0/search/experience.js
+++ routes/v0/search/experience.js
@@ -1,6 +1,9 @@
+const { BEGINNER, FAMILIAR, EXPERIENCED, EXPERT } = require("../../../shared/utils/flags");
+
+
 module.exports = (req, res, next) => {
     //hardcode 10000 miles in for now for a limit
-    const names = res.locals.names;
+    const names = req.query.pursuit;
     const users = res.locals.users;
     const beginner = [];
     const familiar = [];
@@ -8,21 +11,33 @@ module.exports = (req, res, next) => {
     const expert = [];
 
     for (const user of users) {
+        // console.log(user);
         for (let i = 1; i < user.pursuits.length; i++) {
-            if (names.includes(user.pursuits[i])) {
-                switch (pursuits[i].experience_level) {
+            console.log(names.includes(user.pursuits[i].name));
+            if (names.includes(user.pursuits[i].name)) {
+                const data = {
+                    parent_index_user_id: user.parent_index_user_id,
+                    parent_user_id: user.parent_user_id,
+                    username: user.username,
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    distance: user.distance,
+                    small_cropped_display_photo_key: user.small_cropped_display_photo_key,
+                    tiny_cropped_display_photo_key: user.tiny_cropped_display_photo_key,
+                    pursuit: user.pursuits[i]
+                };
+                switch (user.pursuits[i].experience_level) {
                     case (BEGINNER):
-                        if (beginner.length)
-                        beginner.push(user);
+                        beginner.push(data);
                         break;
                     case (FAMILIAR):
-                        familiar.push(user);
+                        familiar.push(data);
                         break;
                     case (EXPERIENCED):
-                        experienced.push(user);
+                        experienced.push(data);
                         break;
                     case (EXPERT):
-                        expert.push(user);
+                        expert.push(data);
                         break;
                     default:
                         break;
--- routes/v0/search/peopleFinder.js
+++ routes/v0/search/peopleFinder.js
@@ -3,12 +3,12 @@ const searchServices = require('./services');
 module.exports = (req, res, next) => {
     const distance = req.query.distance;
     const userPreviewIDList = req.query.userPreviewIDList ? req.query.userPreviewIDList : [];
-    const pursuit = req.query.pursuit;
+    const pursuits = req.query.pursuit;
     const lat = req.query.latitude;
     const long = req.query.longitude;
     const limits = searchServices.getBounds(distance, { lat, long });
     return searchServices
-        .searchByBoundedPursuits(userPreviewIDList, limits, pursuit)
+        .searchByBoundedPursuits(userPreviewIDList, limits, pursuits)
         .then(results => {
             res.locals.users = results.map(value => {
                 value['distance'] = searchServices
