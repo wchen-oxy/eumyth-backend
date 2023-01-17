6008eea1afb980cd8c632910dcc9674cede4d3ea
William S Chen <blackjamx5@hotmail.com>
2023-01-17 13:31:47 -0800

    commented out cached

--- models/cached.project.preview.model.js
+++ models/cached.project.preview.model.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const CachedProjectPreviewSchema = new Schema({
-    project_preview_id: mongoose.Types.ObjectId,
+// const CachedProjectPreviewSchema = new Schema({
+//     project_preview_id: mongoose.Types.ObjectId,
 
 
-})
+// })
 
-const CachedProjectPreviewModel = mongoose.model('cachedProjectPreview', CachedProjectPreviewSchema);
+// const CachedProjectPreviewModel = mongoose.model('cachedProjectPreview', CachedProjectPreviewSchema);
 
-module.exports = {
-    Schema: CachedProjectPreviewSchema,
-    Model: CachedProjectPreviewModel
+// module.exports = {
+//     Schema: CachedProjectPreviewSchema,
+//     Model: CachedProjectPreviewModel
 
-};
+// };
