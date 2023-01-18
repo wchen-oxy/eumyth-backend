b70584d77dd62f9b68db373a0cb461fb530c15df
William S Chen <blackjamx5@hotmail.com>
2023-01-18 15:11:13 -0800

    added index user id reference

--- routes/v0/relation/services.js
+++ routes/v0/relation/services.js
@@ -2,14 +2,16 @@
 const selectModel = require('../../../models/modelServices.js')
 const ModelConstants = require('../../../models/constants');
 
-const _statusChanger = (action, targetID, array, isPrivate) => {
-    const stringID = targetID.toString();
+const _statusChanger = (action, target, array, isPrivate) => {
+    const indexUserID = target.index_user_id;
+    const userPreviewID = target.user_preview_id;
     switch (action) {
         case ("FOLLOW"):
             const state = isPrivate ? "FOLLOW_REQUESTED" : "FOLLOWING";
             array.push(selectModel(ModelConstants.USER_RELATION_STATUS)({
                 status: state,
-                user_preview_id: targetID,
+                index_user_id: indexUserID,
+                user_preview_id: userPreviewID,
             }));
         case ("DECLINE"):
             return array.filter(item => item.user_preview_id.toString() !== stringID);
@@ -36,12 +38,12 @@ exports.setAction = (
     isPrivate) => {
     console.log(action);
     if (action === "FOLLOW" || action === "ACCEPT") {
-        _statusChanger(action, targetUserRelation.user_preview_id, visitorUserRelation.following, isPrivate);
-        _statusChanger(action, visitorUserRelation.user_preview_id, targetUserRelation.followers, isPrivate);
+        _statusChanger(action, targetUserRelation, visitorUserRelation.following, isPrivate);
+        _statusChanger(action, visitorUserRelation, targetUserRelation.followers, isPrivate);
     }
     else if ("UNFOLLOW" || 'DECLINE') {
-        visitorUserRelation.following = _statusChanger(action, targetUserRelation.user_preview_id, visitorUserRelation.following);
-        targetUserRelation.followers = _statusChanger(action, visitorUserRelation.user_preview_id, targetUserRelation.followers);
+        visitorUserRelation.following = _statusChanger(action, targetUserRelation, visitorUserRelation.following);
+        targetUserRelation.followers = _statusChanger(action, visitorUserRelation, targetUserRelation.followers);
     }
     else {
         throw new Error('invalid action')
